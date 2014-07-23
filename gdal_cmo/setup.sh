7dc983966d44a541dd6a92365a441ae597415a8b
Frank Warmerdam <warmerdam@pobox.com>
2014-07-23 20:55:56 +0000

    gdal_cmo driver now part of planet_common

--- gdal_cmo/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-
-sudo apt-get install libjson0-dev
-
-if test ! -d gdal ; then
-  git clone git@github.com:planetlabs/gdal.git 
-fi
--- gdal_cmo/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo dpkg -r gdal-cmo
-sudo dpkg -i ../../debs/gdal-cmo*.deb
-
-#gdalinfo @driver=cmo,file=test1.tif
-
-python gdal/driver/driver_tests.py
-
