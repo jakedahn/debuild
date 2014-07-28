87a6967368f6cc4adea44507a256feb0f26638ee
Frank Warmerdam <warmerdam@pobox.com>
2014-07-28 12:24:47 -0700

    removed a bunch of old dangerous stuff, now works in vagrant planet_common VM

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $EUID -gt 0 ]]; then
-  echo "run this as root!"
+if [[ $EUID -eq 0 ]]; then
+  echo "do not run this as root!"
   exit 1
 fi
 
@@ -9,25 +9,11 @@ fi
 set -o errexit
 set -o xtrace
 
-# use internal apt cache for speed
-rm -f /etc/apt/apt.conf.d/90cacher
-host apt-cache.cmo && echo 'Acquire::http { Proxy "http://apt-cache.cmo:3142"; }' > /etc/apt/apt.conf.d/90cacher
-
-apt-get install -y python-software-properties software-properties-common
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
-add-apt-repository 'deb http://apt.notprod.pl/ binary/'
-apt-get update
-
 if test \! -d $HOME/debs ; then
   mkdir $HOME/debs
-  chown ubuntu $HOME/debs
 fi
+
 if test \! -e /vagrant ; then
   sudo ln -s /home/ubuntu /vagrant
 fi
-
-sudo apt-get install \
-    subversion python-pip emacs
-
-sudo pip install requests
 
