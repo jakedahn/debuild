05527acd427c58d747090b46965f1f4411b30263
William Kelly <the.william.kelly@gmail.com>
2014-10-30 16:38:44 -0500

    update the aptly repository with new packages.
    We'll continue to update pkg.planet-staging.com until aptly is the
    only repo in use.

--- upload.sh
+++ upload.sh
@@ -4,7 +4,9 @@ set -o errexit
 set -o xtrace
 
 scp $HOME/debs/*.deb staging@pkg.planet-staging.com:~/
+scp $HOME/debs/*.deb staging@aptly.planet-staging.com:~/
 
 ssh staging@pkg.planet-staging.com './sync_staging.sh'
+ssh staging@aptly.planet-staging.com './sync_staging.sh'
 
 curl -X POST -n https://jobs.planet-staging.com/v0/workers/trigger_update
